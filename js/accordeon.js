65b01488a3fec818e1e8b88f13340ab5e82b7bc8
aotarasenko <beinjinji@gmail.com>
2021-05-22 19:52:46 +0300

    Accordion is created

---
+++ js/accordeon.js
@@ -0,0 +1,15 @@
+const tipButton = document.querySelectorAll('.tips-item-btn');
+const tipContent = document.querySelectorAll('.tips-item-content');
+
+let currentTip = 0
+
+for (let i = 0; i < tipButton.length; i++) {
+    tipButton[i].addEventListener('click', (Event) => {
+        tipContent[i].classList.toggle('open-tip');
+        for (let j = 0; j < tipButton.length; j++) {
+            if(j != i){
+                tipContent[j].classList.remove('open-tip');
+            }
+        }
+    });
+}
