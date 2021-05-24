6b08d7e0d87a2181c0ff28975631d0a2e5bcec8f
aotarasenko <beinjinji@gmail.com>
2021-05-24 19:50:21 +0300

    Chsnging slide by title clicking

--- js/customSlider.js
+++ js/customSlider.js
@@ -2,13 +2,19 @@ const sliderBlock = document.querySelector('.slider-block');
 const leftSliderArrow = document.querySelector('.arrow-left');
 const rightSliderArrow = document.querySelector('.arrow-right');
 let sliderList = document.querySelectorAll('.slider-list-item');
+
 const sliderSourse = ['0.jpg','1.jpg','2.jpg','3.jpg', '4.jpg'];
 
 
 let currentSlide = 1;
 
 function changeSlide(index) {
-    console.log(index);
+    if(currentSlide < 0){
+        currentSlide = sliderSourse.length-1;
+    }
+    if(currentSlide == sliderSourse.length){
+        currentSlide = 0;
+    }
     sliderBlock.style.backgroundImage = `url('./images/slider-img-${sliderSourse[index]}')`;
     for (let i = 0; i < sliderList.length; i++) {
         if(i == index){
@@ -21,24 +27,22 @@ function changeSlide(index) {
 }
 
 setInterval(()=>{
-    if(currentSlide == sliderSourse.length){
-        currentSlide = 0;
-    }
     changeSlide(currentSlide++);
 }, 5000);
 
 
 leftSliderArrow.addEventListener('click', () => {
-    if(currentSlide < 0){
-        currentSlide = sliderSourse.length-1;
-    }
     changeSlide(currentSlide--);
 });
 
 
 rightSliderArrow.addEventListener('click', () => {
-    if(currentSlide == sliderSourse.length){
-        currentSlide = 0;
-    }
     changeSlide(currentSlide++);
 });
+
+for (let i = 0; i < sliderList.length; i++) {
+    sliderList[i].addEventListener('click', () => {
+        currentSlide = i;
+        changeSlide(currentSlide++);
+    });
+}
