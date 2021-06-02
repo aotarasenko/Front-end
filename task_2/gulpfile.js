0ad5c5988c71ff300ed3a963b2dc1b835068e18b
aotarasenko <beinjinji@gmail.com>
2021-06-02 17:59:47 +0300

    Created header section

--- task_2/gulpfile.js
+++ task_2/gulpfile.js
@@ -4,15 +4,15 @@ const sourcemaps = require('gulp-sourcemaps');
 const watch = require('gulp-watch');
 const concat = require('gulp-concat');
 
-gulp.task('sass-compile', () => {
-    return gulp.src('./scss/**/*.scss')
+gulp.task('sass-compile', () =>{
+    return gulp.src('./scss/main.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css/'));
 });
 
-gulp.task('watch', () => {
+gulp.task('watch', () =>{
     gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'));
     gulp.watch('./js/**/*.js', gulp.series('scripts'));
 });
---
+++ task_2/js/index.js
@@ -0,0 +1,22 @@
+const burgerMenu = document.querySelector(".nav-menu-active");
+const menuList = document.querySelector(".nav-list");
+const header = document.querySelector("header");
+const navBlock = document.querySelector(".nav");
+
+burgerMenu.addEventListener("click", () =>{
+  menuList.classList.toggle("visible-list");
+  burgerMenu.classList.toggle("open");
+  if (menuList.classList.contains("visible-list")) {
+    document.body.classList.add("no-scroll");
+  } else {
+    document.body.classList.remove("no-scroll");
+  }
+});
+
+window.addEventListener("scroll", () =>{
+  if (window.pageYOffset >1) {
+    navBlock.classList.add("fixed-block");
+  } else {
+    navBlock.classList.remove("fixed-block");
+  }
+});
