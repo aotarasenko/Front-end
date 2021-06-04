2a7aba53ea2f39a5eb4d6b221cee38bda66ccd58
aotarasenko <beinjinji@gmail.com>
2021-06-04 22:45:08 +0300

    Include slickjs slider

--- task_2/gulpfile.js
+++ task_2/gulpfile.js
@@ -12,13 +12,13 @@ gulp.task('sass-compile', () =>{
     .pipe(gulp.dest('./css/'));
 });
 
-gulp.task('watch', () =>{
-    gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'));
-    gulp.watch('./js/**/*.js', gulp.series('scripts'));
-});
-
 gulp.task('scripts', function() {
     return gulp.src('./js/*.js')
     .pipe(concat('all.js'))
     .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('watch', () =>{
+    gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'));
+    gulp.watch('./js/**/*.js', gulp.series('scripts'));
 });
---
+++ task_2/js/customSlider.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        nextArrow: false,
+        prevArrow: false,
+        autoplaySpeed: 30000,
+        dots: true,
+    });
+});
