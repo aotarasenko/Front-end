087d0b8b5464e6572ab582ce4ccd002a0ab87cee
aotarasenko <beinjinji@gmail.com>
2021-06-06 13:24:45 +0300

    Completed first slider

--- task_2/js/customSlider.js
+++ task_2/js/customSlider.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
     $('.slider').slick({
         nextArrow: false,
         prevArrow: false,
-        autoplaySpeed: 30000,
+        autoPlay: true,
+        autoplaySpeed: 300,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        swipe: true,
         dots: true,
     });
+
+    const slickButtons = document.querySelectorAll('.slick-dots li button');
+    for (let i = 0; i < slickButtons.length; i++) {
+      slickButtons[i].textContent = '';
+    }
 });
