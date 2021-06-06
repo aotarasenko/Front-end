e4afeb3f26c7340a5b8696509c85187c00b3a50b
aotarasenko <beinjinji@gmail.com>
2021-06-06 14:03:47 +0300

    Complete second slider

--- task_2/js/customSlider.js
+++ task_2/js/customSlider.js
@@ -3,12 +3,26 @@ $(document).ready(function(){
         nextArrow: false,
         prevArrow: false,
         autoPlay: true,
-        autoplaySpeed: 300,
+        autoplaySpeed: 30000,
         slidesToScroll: 1,
         slidesToShow: 1,
         swipe: true,
         dots: true,
     });
+
+    $('.plans-list').slick({
+        nextArrow: false,
+        prevArrow: false,
+        autoPlay: false,
+        slidesToScroll: 1,
+        slidesToShow: 1.1,
+        swipe: true,
+        dots: false,
+    });
+
+    if(window.matchMedia('(min-width: 992px)').matches){
+        $('.plans-list').slick('unslick');
+    }
 
     const slickButtons = document.querySelectorAll('.slick-dots li button');
     for (let i = 0; i < slickButtons.length; i++) {
