d7aadfd6925952a19362de9e98f66d711e5b5f53
aotarasenko <beinjinji@gmail.com>
2021-06-16 13:17:12 +0300

    Chenged some styles ordering like box model

--- task_2/js/customSlider.js
+++ task_2/js/customSlider.js
@@ -1,42 +1,41 @@
 $(document).ready(function () {
-    $('.slider').slick({
-        nextArrow: false,
-        prevArrow: false,
-        autoPlay: true,
-        autoplaySpeed: 3000,
-        slidesToScroll: 1,
-        slidesToShow: 1,
-        swipe: true,
-        dots: true,
-    })
+  $('.slider').slick({
+    nextArrow: false,
+    prevArrow: false,
+    autoPlay: true,
+    autoplaySpeed: 3000,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    swipe: true,
+    dots: true,
+  })
 
-    $('.plans-list').slick({
-        nextArrow: false,
-        prevArrow: false,
-        autoPlay: false,
-        slidesToScroll: 1,
-        slidesToShow: 1.1,
-        swipe: true,
-        dots: false,
-    })
+  $('.plans-list').slick({
+    nextArrow: false,
+    prevArrow: false,
+    autoPlay: false,
+    slidesToScroll: 1,
+    slidesToShow: 1.1,
+    swipe: true,
+    dots: false,
+  })
 
-    $('.partners-slider').slick({
-        nextArrow: false,
-        prevArrow: false,
-        autoPlay: false,
-        slidesToScroll: 1,
-        slidesToShow: 1,
-        swipe: true,
-        dots: true,
-    })
+  $('.partners-slider').slick({
+    nextArrow: false,
+    prevArrow: false,
+    autoPlay: false,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    swipe: true,
+    dots: true,
+  })
 
+  if (window.matchMedia('(min-width: 1024px)').matches) {
+    $('.plans-list').slick('unslick')
+  }
 
-    if (window.matchMedia('(min-width: 1024px)').matches) {
-        $('.plans-list').slick('unslick')
-    }
-
-    const slickButtons = document.querySelectorAll('.slick-dots li button')
-    for (let i = 0; i < slickButtons.length; i++) {
-        slickButtons[i].textContent = ''
-    }
+  const slickButtons = document.querySelectorAll('.slick-dots li button')
+  for (let i = 0; i < slickButtons.length; i++) {
+    slickButtons[i].textContent = ''
+  }
 })
--- task_2/js/index.js
+++ task_2/js/index.js
@@ -2,24 +2,21 @@ const burgerMenu = document.querySelector('.nav-menu-active')
 const menuList = document.querySelector('.nav-list')
 const header = document.querySelector('header')
 const navBlock = document.querySelector('.nav')
-const partnersSliderButtons = document.querySelectorAll('.partners-slider > .slick-dots li')
-
-console.log(partnersSliderButtons);
 
 burgerMenu.addEventListener('click', () => {
-    menuList.classList.toggle('visible-list')
-    burgerMenu.classList.toggle('open')
-    if (menuList.classList.contains('visible-list')) {
-        document.body.classList.add('no-scroll')
-    } else {
-        document.body.classList.remove('no-scroll')
-    }
+  menuList.classList.toggle('visible-list')
+  burgerMenu.classList.toggle('open')
+  if (menuList.classList.contains('visible-list')) {
+    document.body.classList.add('no-scroll')
+  } else {
+    document.body.classList.remove('no-scroll')
+  }
 })
 
 window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 1) {
-        navBlock.classList.add('fixed-block')
-    } else {
-        navBlock.classList.remove('fixed-block')
-    }
+  if (window.pageYOffset > 1) {
+    navBlock.classList.add('fixed-block')
+  } else {
+    navBlock.classList.remove('fixed-block')
+  }
 })
