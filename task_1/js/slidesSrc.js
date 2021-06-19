7ee94ee0461f23bd41fef27a15d379cf8a7e1a2a
aotarasenko <beinjinji@gmail.com>
2021-06-19 12:14:21 +0300

    Added first task dir

---
+++ task_1/js/slidesSrc.js
@@ -0,0 +1,134 @@
+const slides = [
+  {
+    title: "Botnet",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-0.jpg",
+  },
+  {
+    title: "Breach",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-1.jpg",
+  },
+  {
+    title: "Cloud",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-2.jpg",
+  },
+  {
+    title: "DDoS",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-3.jpg",
+  },
+  {
+    title: "Domain",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-4.jpg",
+  },
+  {
+    title: "Endpoint",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-0.jpg",
+  },
+  {
+    title: "Exploid",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-4.jpg",
+  },
+  {
+    title: "Firewall",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-1.jpg",
+  },
+  {
+    title: "IP Address",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-2.jpg",
+  },
+  {
+    title: "Malware",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-3.jpg",
+  },
+  {
+    title: "Penetration Test",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-4.jpg",
+  },
+  {
+    title: "Phishing/Spearphishi",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-0.jpg",
+  },
+  {
+    title: "ng",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-1.jpg",
+  },
+  {
+    title: "Reasomware",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-2.jpg",
+  },
+  {
+    title: "Session hijacking",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-3.jpg",
+  },
+  {
+    title: "Social Engineering",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-4.jpg",
+  },
+  {
+    title: "Software",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-3.jpg",
+  },
+  {
+    title: "SOC",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-4.jpg",
+  },
+  {
+    title: "Trojan Horse",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-0.jpg",
+  },
+  {
+    title: "VPN",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-1.jpg",
+  },
+  {
+    title: "Virus",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-2.jpg",
+  },
+  {
+    title: "Worm",
+    description:
+      "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. ",
+    img: "./images/slider-img-3.jpg",
+  },
+];
