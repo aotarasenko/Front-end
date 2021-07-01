4a3bc2a2c0d9827fb6dde7dd38aeb9044a528ed4
aotarasenko <beinjinji@gmail.com>
2021-07-01 13:00:25 +0300

    Added function for output line and output symbols to ui

--- js_task1/firstTask.js
+++ js_task1/firstTask.js
@@ -1,19 +1,27 @@
 const input = document.querySelector(".first-task input");
 const pushButton = document.querySelector(".first-task button");
-const symbol = "$";
+const outArea = document.querySelector(".first-task .out-area");
+const symbol = " $ "; // out symbol
+
+function outputLine(symbolCount) {
+  outArea.innerHTML += `${symbol.repeat(symbolCount)} <br>`;
+}
 
 pushButton.addEventListener("click", () => {
+  outArea.innerHTML = "";
   let number = Math.round(Number(input.value));
   if (isNaN(number) || number <= 0) {
     alert("Incorrect value");
   } else {
     for (let i = 1; number > 0; i++) {
       if (number <= i) {
-        console.log(symbol.repeat(number));
-        console.log("rows", i);
+        // outArea.innerHTML += `${symbol.repeat(number)} <br>`;
+        outputLine(number);
+        outArea.innerHTML += `Rows: ${i}`;
         break;
       }
-      console.log(symbol.repeat(i));
+      // outArea.innerHTML += `${symbol.repeat(i)} <br>`;
+      outputLine(i);
       number -= i;
     }
   }
