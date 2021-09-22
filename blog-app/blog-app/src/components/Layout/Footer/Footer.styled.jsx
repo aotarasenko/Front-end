bf87ee55ae9b037fd6227a3c46119d335c4ebebe
aotarasenko <beinjinji@gmail.com>
2021-09-22 17:03:29 +0300

    Updates

--- blog-app/blog-app/src/components/Layout/Footer/Footer.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { AppColors, AppFontSizes } from "../../styles/variables";
-
-export const Copyright = styled.footer`
-  padding: 20px 0;
-  color: ${AppColors.white};
-  font-size: ${AppFontSizes.small};
-`;
